5d645f0929bfd170204491b2e0cce23b599228df
Rutuja Patil <patilrut@amazon.com>
2024-06-20 13:08:34 +0530

    Fixing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import LandingPage from './components/pages/LandingPage'
 import LoginPage from './components/pages/LoginPage'
@@ -10,36 +10,18 @@ import HomePage from './components/pages/HomePage'
 import './App.css'
 
 export default function App() {
+    console.log(LandingPage)
     return (
         <Router>
             <div>
-                <Switch>
-                    <Route exact path="/" component={ LandingPage } />
-                    <Route path="/login" component={ LoginPage } />
-                    <Route path="/register" component={ RegisterPage } />
-                    <Route path="/forget-password" component={ ForgetPasswordPage } />
-                    <Route path="/home" component={ HomePage } />
-                </Switch>
-                <Footer />
+                <Routes>
+                    <Route exact path="/" element={ <LandingPage /> } />
+                    <Route path="/login" element={ <LoginPage /> } />
+                    <Route path="/register" element={ <RegisterPage /> } />
+                    <Route path="/forget-password" element={ <ForgetPasswordPage /> } />
+                    <Route path="/home" element={ <HomePage /> } />
+                </Routes>
             </div>
         </Router>
     )
-}
-
-const Footer = () => {
-    return (
-        <p className="text-center" style={ FooterStyle }>Designed & coded by <a href="https://izemspot.netlify.com" target="_blank" rel="noopener noreferrer">IZEMSPOT</a></p>
-    )
-}
-
-const FooterStyle = {
-    background: "#222",
-    fontSize: ".8rem",
-    color: "#fff",
-    position: "absolute",
-    bottom: 0,
-    padding: "1rem",
-    margin: 0,
-    width: "100%",
-    opacity: ".5"
 }
--- src/components/pages/LandingPage.js
+++ src/components/pages/LandingPage.js
@@ -2,13 +2,10 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 import '../../App.css'
-import BackgroundImage from '../../assets/images/bg.png'
-
 export default function LandingPage() {
     return (
         <header style={ HeaderStyle }>
             <h1 className="main-title text-center">login / register page</h1>
-            <p className="main-para text-center">join us now and don't waste time</p>
             <div className="buttons text-center">
                 <Link to="/login">
                     <button className="primary-button">log in</button>
@@ -24,7 +21,6 @@ export default function LandingPage() {
 const HeaderStyle = {
     width: "100%",
     height: "100vh",
-    background: `url(${BackgroundImage})`,
     backgroundPosition: "center",
     backgroundRepeat: "no-repeat",
     backgroundSize: "cover"
